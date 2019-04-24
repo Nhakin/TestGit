73f1e1bd79294c8ce4f50c2e928ddecbcb32b2d1
Nhakin <kahnabyss1@gmail.com>
2019-04-24 09:44:11 -0400

    Initial Commit

---
+++ TestGit.dpr
@@ -0,0 +1,14 @@
+program TestGit;
+
+uses
+  Forms,
+  TestGitMain in 'TestGitMain.pas' {Form7};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm7, Form7);
+  Application.Run;
+end.
---
+++ TestGitMain.pas
@@ -0,0 +1,31 @@
+unit TestGitMain;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm7 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form7: TForm7;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm7.Button1Click(Sender: TObject);
+begin
+  ShowMessage('Live');
+end;
+
+end.
